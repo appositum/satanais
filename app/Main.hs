8bcef3c382277f8cbe6b8c15893911e212da5d29
appositum <appositum@protonmail.com>
2019-04-25 03:45:32 -0300

    Interpreter module

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
-import Data.Void (Void)
-import Text.Megaparsec
-import Text.Megaparsec.Char
-
-type Parser = Parsec Void String
+import Control.Monad
+import System.IO
+import Satanais
 
 main :: IO ()
-main = pure ()
+main = repl
+
+repl :: IO ()
+repl = do
+  input <- putStr "Satanais> " *> hFlush stdout *> getLine
+  unless (input == ":quit" || input == ":q") $ do
+    if null input
+      then repl
+      else putStrLn input *> repl
