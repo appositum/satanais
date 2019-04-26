3dd86e3d276a77cfd1cac6417f1bf9731db296dd
eddie <appositum@protonmail.com>
2019-04-26 00:04:31 -0300

    Implement expression evaluation on REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,6 @@
 module Main where
 
-import Control.Monad
-import System.IO
 import Satanais
 
 main :: IO ()
 main = repl
-
-repl :: IO ()
-repl = do
-  input <- putStr "Satanais> " *> hFlush stdout *> getLine
-  unless (input == ":quit" || input == ":q") $ do
-    if null input
-      then repl
-      else putStrLn input *> repl
