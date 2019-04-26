2f262ab2a95e959e94d9e65b28887b61b4697962
eddie <appositum@protonmail.com>
2019-04-25 23:02:21 -0300

    Add eval module to default Satanais import

--- src/Satanais.hs
+++ src/Satanais.hs
@@ -1,11 +1,13 @@
 module Satanais
   ( module Satanais.AST
+  , module Satanais.Eval
   , module Satanais.Parser
   , module Text.Megaparsec
   , module Text.Megaparsec.Char
   ) where
 
 import Satanais.AST
+import Satanais.Eval
 import Satanais.Parser
 import Text.Megaparsec
 import Text.Megaparsec.Char
