ef8436c67816edf19b0104a88947670f4b4c27ba
eddie <appositum@protonmail.com>
2019-04-25 22:20:15 -0300

    Repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad
+import System.IO
 import Satanais
 
 main :: IO ()
-main = print 5
+main = repl
+
+repl :: IO ()
+repl = do
+  input <- putStr "Satanais> " *> hFlush stdout *> getLine
+  unless (input == ":quit" || input == ":q") $ do
+    if null input
+      then repl
+      else putStrLn input *> repl
